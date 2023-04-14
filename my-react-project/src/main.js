e6c75c636e3ddcddcee736b78985099b110b8701
Ben A <ben@sol3.me>
2023-04-14 21:01:21 +0100

    Fix buttons not working and just bad render in general

--- my-react-project/src/main.js
+++ my-react-project/src/main.js
@@ -1,60 +1,59 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './app.css';
-import { App } from './app';
+import React from "react";
+import * as ReactDOMClient from "react-dom/client";
+import "./app.css";
+import { App } from "./app";
 
 export const quotes = [
-    { quote: "People like fast cars, females with big boobies and they don't want the Euro, that's all there is to it.", character: "Daryl", season: 2, episode: "Jeremy Makes It" },
-    { quote: "And our ambition should be to kill clients. I mean actually kill them.", character: "Johnson", season: 2, episode: "Local Zero" },
-    { quote: "This children's play centre is your designated snake handover arena?", character: "Mark", season: 9, episode: "Kid Farm" }
+	{ quote: "People like fast cars, females with big boobies and they don't want the Euro, that's all there is to it.", character: "Daryl", season: 2, episode: "Jeremy Makes It" },
+	{ quote: "And our ambition should be to kill clients. I mean actually kill them.", character: "Johnson", season: 2, episode: "Local Zero" },
+	{ quote: "This children's play centre is your designated snake handover arena?", character: "Mark", season: 9, episode: "Kid Farm" },
 ];
 
 const maxGuesses = 8;
 
 export const WordDisplay = ({ currentWord, currentGuesses, quote }) => {
-  const displayQuote = quote.split(' ').map((word, index) => {
-    const strippedWord = word.replace(/[^\w\s]/g, ''); // Strip out punctuation
-    const isCurrentWord = strippedWord.toLowerCase() === currentWord.toLowerCase();
-    const displayWord = strippedWord
-      .split('')
-      .map((letter) =>
-        currentGuesses.includes(letter.toLowerCase()) ? letter.toUpperCase() : '_'
-      )
-      .join('');
+	const displayQuote = quote.split(" ").map((word, index) => {
+		const strippedWord = word.replace(/[^\w\s]/g, ""); // Strip out punctuation
+		const isCurrentWord = strippedWord.toLowerCase() === currentWord.toLowerCase();
+		const displayWord = strippedWord
+			.split("")
+			.map((letter) => (currentGuesses.includes(letter.toLowerCase()) ? letter.toUpperCase() : "_"))
+			.join("");
 
-    return (
-      <span key={index}>
-        {isCurrentWord ? (
-          <span className="current-word">{displayWord}</span>
-        ) : (
-          <span>{word}</span>
-        )}
-        {index !== quote.split(' ').length - 1 && ' '}
-      </span>
-    );
-  });
+		return (
+			<span key={index}>
+				{isCurrentWord ? <span className="current-word">{displayWord}</span> : <span>{word}</span>}
+				{index !== quote.split(" ").length - 1 && " "}
+			</span>
+		);
+	});
 
-  return <div id="word-display">{displayQuote}</div>;
+	return <div id="word-display">{displayQuote}</div>;
 };
 
-
 export const GuessesDisplay = ({ numGuesses }) => {
-  const guessesRemaining = maxGuesses - numGuesses;
+	const guessesRemaining = maxGuesses - numGuesses;
 
-  if (guessesRemaining <= 0) {
-    return <div id="guesses">Sorry, you have no guesses left!</div>;
-  }
+	if (guessesRemaining <= 0) {
+		return <div id="guesses">Sorry, you have no guesses left!</div>;
+	}
 
-  return <div id="guesses">{guessesRemaining} guesses remaining</div>;
+	return <div id="guesses">{guessesRemaining} guesses remaining</div>;
 };
 
 export const NewGameButton = ({ onClick }) => {
-  return <button onClick={onClick}>New Game</button>;
+	return <button onClick={onClick}>New Game</button>;
 };
 
 // Only for debug use - remove later
 export const CurrentWordDisplay = ({ currentWord }) => {
-    return <div id="current-word-display">Current Word: {currentWord}</div>;
-  };
+	return <div id="current-word-display">Current Word: {currentWord}</div>;
+};
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+
+// Create a root.
+const root = ReactDOMClient.createRoot(container);
+
+// Initial render: Render an element to the root.
+root.render(<App />);
